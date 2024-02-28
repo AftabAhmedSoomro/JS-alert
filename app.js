5e9543ad2b5847dd0ce1fd5aae6728f676c253a8
AftabAhmedSoomro <154562779+AftabAhmedSoomro@users.noreply.github.com>
2024-02-28 07:13:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// ALERT
+
+//Q 1st alert
+alert("Shab Bakhair mere pyare bhaiyon!");
+
+//Q 2nd alert
+alert("Error!Please enter a valid password.");
+
+//Q 3rd alert
+alert("Welcome to JS Land...\nHappy Coding!");
+
+//Q 4th alert
+alert("Welcome to JS Land");
+alert("Happy Coding");
+
+//Q 5th alert
+alert("Hello... I can run JS through my web browser's console"); 
+console.log("Hello... I can run JS through my web browser's console");
+
+
+// SWAL 
+
+//Q 1st swal 
+Swal.fire("Kese ana huwa!");
+
+//Q 2nd swal
+Swal.fire({title:"Are You Still Here?",text:"Bhago yahan se!"});
+
+//Q 3rd swal
+Swal.fire({
+    title:"Are You 18+",
+    text:"If Yes then Click Ok"
+}) 
+
+//Q 4th swal
+Swal.fire("Unexpected error occurs\n404 error");
+
+//Q 5th swal
+Swal.fire({
+    title:"Oops...",
+    text:"Something went wrong!"
+  });
